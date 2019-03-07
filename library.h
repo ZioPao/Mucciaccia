31e93ac500509200bea79d60b5d3a68f7496657c
ziopao <picchetti1998@gmail.com>
2019-03-07 20:07:29 +0100

    Salviamo la spazzatura

---
+++ library.h
@@ -0,0 +1,27 @@
+#ifndef MUCCIACCIAGAME_LIBRARY_H
+#define MUCCIACCIAGAME_LIBRARY_H
+
+#include "library.h"
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+#include <set>
+
+#define NUM_OBJECTS 4
+#define MAX_PENIS 25
+
+///Frasi per le fights
+#define PRE_PHRASE1 "Il pene di "
+#define PRE_PHRASE2 "L'enorme fallo di "
+#define PRE_PHRASE3 "Con una spinta poderosa il cazzo di "
+#define POST_PHRASE1 " ha eiaculato nell'ano di "
+#define POST_PHRASE2 " ha penetrato forzatamente tutti gli orifizi di "
+#define POST_PHRASE3 " si è insinuato nelle cavita rettali di "
+
+enum Objects{COLLA,FORBICI,CARTA,MALATTIE};
+enum Condition{WIN, LOSE, TIE};
+
+
+int main();
+#endif
